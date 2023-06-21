7ac1d347eb51e8cf7a78c84c4980b406e4f5ff2a
Jenniferplusplus <contact@jenniferplusplus.com>
2023-06-21 10:49:31 -0500

    added activitypub endpoint stubs

--- Letterbook.Api/Controllers/ActivityPub/ActorController.cs
+++ Letterbook.Api/Controllers/ActivityPub/ActorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.DataProtection.Internal;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Letterbook.Api.Controllers.ActivityPub;
 
@@ -7,9 +8,65 @@ namespace Letterbook.Api.Controllers.ActivityPub;
 public class ActorController
 {
     [HttpGet]
-    [Route("{id}/[action]")]
-    public void GetActor(int id)
+    [Route("{id}")]
+    public IActionResult GetActor(int id)
     {
-        
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet]
+    [Route("{id}/collections/[action]")]
+    public IActionResult GetFollowers(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet]
+    [Route("{id}/collections/[action]")]
+    public IActionResult GetFollowing(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet]
+    [Route("{id}/collections/[action]")]
+    public IActionResult GetLiked(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet]
+    [Route("{id}/inbox")]
+    public IActionResult GetInbox(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost]
+    [Route("{id}/inbox")]
+    public IActionResult PostInbox(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Route("[action]")]
+    public IActionResult SharedInbox()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet]
+    [Route("{id}/outbox")]
+    public IActionResult GetOutbox(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost]
+    [Route("{id}/outbox")]
+    public IActionResult PostOutbox(int id)
+    {
+        throw new NotImplementedException();
     }
 }
---
+++ Letterbook.Api/Controllers/ActivityPub/ObjectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Letterbook.Api.Controllers.ActivityPub;
+
+[ApiController]
+[Route("[controller]/{type}")]
+public class ObjectController
+{
+    [HttpGet]
+    [Route("{id}/collections/[action]")]
+    public IActionResult GetLikes(int id, string type)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet]
+    [Route("{id}/collections/[action]")]
+    public IActionResult GetShares(int id, string type)
+    {
+        throw new NotImplementedException();
+    }
+}
