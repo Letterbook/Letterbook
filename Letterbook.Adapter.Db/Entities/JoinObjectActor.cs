481303d6d213874d6e2c212147c147ad41eb3b0d
Jenniferplusplus <contact@jenniferplusplus.com>
2023-07-13 20:46:35 -0500

    Clearing up most nullability warnings

--- Letterbook.Adapter.Db/Entities/JoinObjectActor.cs
+++ Letterbook.Adapter.Db/Entities/JoinObjectActor.cs
@@ -2,6 +2,13 @@
 
 public class JoinObjectActor
 {
+    private JoinObjectActor()
+    {
+        ObjectId = null!;
+        ActorId = null!;
+        Relationship = AddressedRelationship.None;
+    }
+    
     public Uri ObjectId { get; set; }
     public Uri ActorId { get; set; }
     public AddressedRelationship Relationship { get; set; }
--- Letterbook.Core/Models/ApObject.cs
+++ Letterbook.Core/Models/ApObject.cs
@@ -2,16 +2,24 @@
 
 public class ApObject
 {
+    private ApObject()
+    {
+        Id = default!;
+        Type = ActivityObjectType.None;
+        Host = default!;
+        Actor = default!;
+    }
+
     public Uri Id { get; set; }
     public ActivityObjectType Type { get; set; }
     public string? LocalId { get; set; }
     public Uri Host { get; set; }
     public Actor Actor { get; set; }
-    public List<Actor> AddressedTo { get; set; } = new List<Actor>();
-    public List<Actor> AddressedBto { get; set; } = new List<Actor>();
-    public List<Actor> AddressedCc { get; set; } = new List<Actor>();
-    public List<Actor> AddressedBcc { get; set; } = new List<Actor>();
-    public List<Audience> Audience { get; set; } = new List<Audience>();
+    public List<Actor> AddressedTo { get; set; } = new();
+    public List<Actor> AddressedBto { get; set; } = new();
+    public List<Actor> AddressedCc { get; set; } = new();
+    public List<Actor> AddressedBcc { get; set; } = new();
+    public List<Audience> Audience { get; set; } = new();
     // TODO: content (languages are hard)
     // TODO: versions (ugh)
     // TODO: dates (versions make this harder)
