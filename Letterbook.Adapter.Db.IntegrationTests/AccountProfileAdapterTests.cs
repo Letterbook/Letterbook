d42a0218819244672fd114d3ebd98ae0eb9a3338
Jenniferplusplus <contact@jenniferplusplus.com>
2023-09-24 00:04:05 -0500

    Scaffold AccountProfile integration tests

---
+++ Letterbook.Adapter.Db.IntegrationTests/AccountProfileAdapterTests.cs
@@ -0,0 +1,34 @@
+﻿using Letterbook.Adapter.Db.IntegrationTests.Fixtures;
+using Letterbook.Core.Tests.Fakes;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit.Abstractions;
+
+namespace Letterbook.Adapter.Db.IntegrationTests;
+
+public class AccountProfileAdapterTests : IClassFixture<PostgresFixture>
+{
+    private readonly ITestOutputHelper _output;
+    private readonly PostgresFixture _postgres;
+    private AccountProfileAdapter _adapter;
+    private RelationalContext _context;
+    private RelationalContext _actual;
+    private FakeProfile _fakeProfile;
+    private FakeAccount _fakeAccount;
+    
+    public AccountProfileAdapterTests(ITestOutputHelper output, PostgresFixture postgres)
+    {
+        _output = output;
+        _postgres = postgres;
+
+        _context = _postgres.CreateContext();
+        _actual = _postgres.CreateContext();
+        _adapter = new AccountProfileAdapter(Mock.Of<ILogger<AccountProfileAdapter>>(), _context);
+        
+        _output.WriteLine($"Bogus Seed: {Init.WithSeed()}");
+    }
+    
+    [Fact]
+    public void Exists()
+    {}
+}
---
+++ Letterbook.Adapter.Db.IntegrationTests/Fixtures/PostgresFixture.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Letterbook.Adapter.Db.IntegrationTests.Fixtures;
+
+public class PostgresFixture
+{
+    private const string ConnectionString =
+        @"Server=localhost;Port=5432;Database=letterbook_feeds;User Id=letterbook;Password=letterbookpw;SSL Mode=Disable;Search Path=public";
+    private static readonly object _lock = new();
+    private static bool _databaseInitialized;
+
+    private IOptions<DbOptions> _opts = Options.Create(new DbOptions { ConnectionString = ConnectionString });
+    public RelationalContext CreateContext() => new RelationalContext(_opts);
+
+    public PostgresFixture()
+    {
+        lock (_lock)
+        {
+            if (_databaseInitialized) return;
+            using (RelationalContext context = CreateContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.Migrate();
+            }
+
+            _databaseInitialized = true;
+        }
+    }
+
+}
