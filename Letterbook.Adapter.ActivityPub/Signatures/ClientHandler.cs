ad448beb8d113f4febdb179bdaa4a3b8830e4fb5
Jenniferplusplus <contact@jenniferplusplus.com>
2023-11-05 01:05:28 -0600

    Minor refactor for readability

--- Letterbook.Adapter.ActivityPub/Signatures/ClientHandler.cs
+++ Letterbook.Adapter.ActivityPub/Signatures/ClientHandler.cs
@@ -22,31 +22,33 @@ public class ClientHandler : DelegatingHandler
         request.Headers.Date ??= DateTimeOffset.Now;
         if (request.Content?.Headers.TryGetValues("Content-Digest", out var digest) == true)
             request.Headers.Add("Digest", digest);
-        if (request.Options.TryGetValue(new HttpRequestOptionsKey<IEnumerable<Models.SigningKey>>(IClientSigner.SigningKeysOptionsId),
+        if (!request.Options.TryGetValue(
+                new HttpRequestOptionsKey<IEnumerable<Models.SigningKey>>(IClientSigner.SigningKeysOptionsId),
                 out IEnumerable<Models.SigningKey>? keys))
         {
-            // TODO: refactor before adding other signers
-            // This works for mastodon, but we want to add spec-compliant signatures in the future
-            
-            // Ideally, this would support Accept-Signature negotiation
-            // And if nothing else, we want at most a small set of signatures covering some common algorithms. Not a
-            // combinatorial set of all keys and algorithms we can possibly support.
-            var key = keys.FirstOrDefault(k => k.Family == Models.SigningKey.KeyFamily.Rsa);
-            if (key == null)
-            {
-                _logger.LogWarning("Can't generate mastodon-structured signature because no RSA keys were available");
-                return base.SendAsync(request, cancellationToken);
-            }
-
-            foreach (var signer in _signers)
-            {
-                signer.SignRequest(request, key);
-            }
-        }
-        else
-        {
             _logger.LogWarning("Can's sign request because no keys were available");
+            _logger.LogDebug("Couldn't sign request to {Destination}", request.RequestUri);
+            return base.SendAsync(request, cancellationToken);
         }
+        
+        // TODO: refactor before adding other signers
+        // This works for mastodon, but we want to add spec-compliant signatures in the future
+
+        // Ideally, this would support Accept-Signature negotiation
+        // And if nothing else, we want at most a small set of signatures covering some common algorithms. Not a
+        // combinatorial set of all keys and algorithms we can possibly support.
+        var key = keys.FirstOrDefault(k => k.Family == Models.SigningKey.KeyFamily.Rsa);
+        if (key == null)
+        {
+            _logger.LogWarning("Can't generate mastodon-structured signature because no RSA keys were available");
+            return base.SendAsync(request, cancellationToken);
+        }
+
+        foreach (var signer in _signers)
+        {
+            signer.SignRequest(request, key);
+        }
+
         return base.SendAsync(request, cancellationToken);
     }
 }
