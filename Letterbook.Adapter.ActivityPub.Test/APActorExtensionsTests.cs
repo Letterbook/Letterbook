a35c7c61713d07d80a61333e5a9150611fa8a69d
Jenniferplusplus <contact@jenniferplusplus.com>
2023-11-06 02:30:12 -0600

    Trying out APSharp

---
+++ Letterbook.Adapter.ActivityPub.Test/APActorExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using ActivityPub.Types.AS;
+using ActivityPub.Types.Conversion;
+using ActivityPub.Types.Internal;
+using Microsoft.Extensions.Options;
+
+namespace Letterbook.Adapter.ActivityPub.Test;
+
+public class APActorExtensionsTests
+{
+    private JsonLdSerializer _serializer => new JsonLdSerializer(Options.Create(new JsonLdSerializerOptions()), new ASTypeInfoCache());
+    
+    [Fact]
+    public void TestSerialize()
+    {
+        var actor = new APActorExtensions()
+        {
+            Inbox = "http://letterbook.example/actor/inbox",
+            Outbox = "http://letterbook.example/actor/outbox",
+            PublicKey = new PublicKey()
+            {
+                Id = "http://letterbook.example/actor/keys/0",
+                PublicKeyPem = @"-----BEGIN PUBLIC KEY-----\nBase64KeyValues\n-----END PUBLIC KEY-----\n"
+            }
+        };
+
+        var json = _serializer.SerializeToElement(actor);
+        
+        Assert.True(json.TryGetProperty("publicKey", out var publicKey), "missing publicKey"); // Fails
+        Assert.True(publicKey.TryGetProperty("publicKeyPem", out var publicKeyPem), "missing publicKeyPem");
+        Assert.Equal(actor.PublicKey.PublicKeyPem, publicKeyPem.GetString());
+        Assert.True(json.TryGetProperty("type", out var type));
+        
+        // It should be some kind of Actor type. How should that be set?
+        Assert.NotEqual("Object", type.GetString());
+        // Also, the publicKey object shouldn't have a `type`, but it does. Should it inherit from something else?
+    }
+
+    [Fact]
+    public void TestDeserialize()
+    {
+        var json = """
+                   {
+                     "type": "Person",
+                     "@context": "https://www.w3.org/ns/activitystreams",
+                     "inbox": "http://letterbook.example/actor/inbox",
+                     "outbox": "http://letterbook.example/actor/outbox",
+                     "publicKey": {
+                       "@context": "https://www.w3.org/ns/activitystreams",
+                       "id": "http://letterbook.example/actor/keys/0",
+                       "publicKeyPem": "-----BEGIN PUBLIC KEY-----\nBase64KeyValues\\n-----END PUBLIC KEY-----\n"
+                     }
+                   }
+                   """;
+        // How would I deserialize an object whose derived type I don't know in advance?
+        var poco = _serializer.Deserialize<ASType>(json); // throws
+        
+        Assert.True(poco?.Is<APActorExtensions>());
+        Assert.NotNull(poco?.As<APActorExtensions>().PublicKey);
+        Assert.Equal(@"-----BEGIN PUBLIC KEY-----\nBase64KeyValues\\n-----END PUBLIC KEY-----\n", poco?.As<APActorExtensions>().PublicKey?.PublicKeyPem);
+    }
+}
---
+++ Letterbook.Adapter.ActivityPub/APActorExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json.Serialization;
+using ActivityPub.Types;
+using ActivityPub.Types.AS;
+using ActivityPub.Types.Attributes;
+using ActivityPub.Types.Util;
+
+namespace Letterbook.Adapter.ActivityPub;
+
+// The goal here is to extend AP actor with the PublicKey property, and have it (de)serialize appropriately
+// Am I on the right track with all this?
+public class APActorExtensions : APActor
+{
+    private APActorExtensionsEntity Entity { get; }
+
+    public APActorExtensions() => Entity = new APActorExtensionsEntity { TypeMap = TypeMap };
+    public APActorExtensions(TypeMap typeMap) : base(typeMap) => Entity = TypeMap.AsEntity<APActorExtensionsEntity>();
+
+    public PublicKey? PublicKey
+    {
+        get => Entity.PublicKey;
+        set => Entity.PublicKey = value!;
+    }
+}
+
+[APConvertible("what goes here?")] // Is this even needed? Do I need one for each Actor type?
+// Is this right?
+[ImpliesOtherEntity(typeof(APActorEntity))]
+public sealed class APActorExtensionsEntity : ASEntity<APActorExtensions>
+{
+    // Does this have to duplicate all of the properties of APActorEntity?
+    
+    [JsonPropertyName("publicKey")]
+    public PublicKey PublicKey { get; set; }
+}
+
+
+// PublicKeys
+
+public class PublicKey : ASObject
+{
+    private PublicKeyEntity Entity { get; }
+
+    public PublicKey() => Entity = new PublicKeyEntity()
+    {
+        TypeMap = TypeMap,
+        Owner = default!,
+        PublicKeyPem = default!
+    };
+    
+    public PublicKey(TypeMap typeMap) : base(typeMap) => Entity = TypeMap.AsEntity<PublicKeyEntity>();
+
+    public Linkable<APActor> Owner
+    {
+        get => Entity.Owner;
+        set => Entity.Owner = value;
+    }
+
+    public string PublicKeyPem
+    {
+        get => Entity.PublicKeyPem;
+        set => Entity.PublicKeyPem = value;
+    }
+}
+
+// PublicKey isn't part of a type set with anything else, so it probably doesn't need ImpliesOtherEntity, right?
+public sealed class PublicKeyEntity : ASEntity<PublicKey>
+{
+    [JsonPropertyName("owner")]
+    public required Linkable<APActor> Owner { get; set; }
+    
+    [JsonPropertyName("publicKeyPem")]
+    public required string PublicKeyPem { get; set; }
+}
