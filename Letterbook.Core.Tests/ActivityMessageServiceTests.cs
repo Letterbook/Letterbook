9a94f519fcc552d9f5487ea0fdb90e06ee48de73
Jenniferplusplus <contact@jenniferplusplus.com>
2023-12-22 14:14:13 -0600

    Added ActivityMessageServiceTests

--- Letterbook.Core.Tests/ActivityMessageServiceTests.cs
+++ Letterbook.Core.Tests/ActivityMessageServiceTests.cs
@@ -1,20 +1,44 @@
-﻿using ActivityPub.Types.Conversion;
-using Letterbook.Core.Adapters;
+﻿using System.Reactive;
+using System.Reactive.Subjects;
+using ActivityPub.Types.AS.Extended.Activity;
+using ActivityPub.Types.Conversion;
+using CloudNative.CloudEvents;
+using Letterbook.Core.Models;
+using Letterbook.Core.Tests.Fakes;
 using Letterbook.Core.Tests.Fixtures;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Xunit.Abstractions;
 
 namespace Letterbook.Core.Tests;
 
 public class ActivityMessageServiceTests : WithMocks, IClassFixture<JsonLdSerializerFixture>
 {
+    private readonly ITestOutputHelper _output;
     private readonly IJsonLdSerializer _serializer;
     private ActivityMessageService _service;
+    private readonly Profile _profile;
+    private readonly Uri _inbox;
+    private readonly AnnounceActivity _activity;
+    private readonly Subject<CloudEvent> _subject;
 
-    public ActivityMessageServiceTests(JsonLdSerializerFixture serializer)
+    public ActivityMessageServiceTests(ITestOutputHelper output, JsonLdSerializerFixture serializer)
     {
+        _output = output;
+        output.WriteLine($"Bogus seed: {Init.WithSeed()}");
         _serializer = serializer.JsonLdSerializer;
-        _service = new ActivityMessageService(Mock.Of<ILogger<ActivityMessageService>>(), CoreOptionsMock, _serializer, Mock.Of<IMessageBusAdapter>());
+
+        var faker = new FakeProfile("letterbook.example");
+        _profile = faker.Generate();
+        _inbox = new Uri("https://peer.example/actor/someactor/inbox");
+        _activity = new AnnounceActivity();
+        _activity.Actor.Add(_profile.Id);
+        _activity.Object.Add(new Uri("https://peer.example/object/12345"));
+        _subject = new Subject<CloudEvent>();
+        MessageBusAdapterMock.Setup(m => m.OpenChannel<It.IsAnyType>(It.IsAny<string?>()))
+            .Returns(_subject.AsObserver());
+        
+        _service = new ActivityMessageService(Mock.Of<ILogger<ActivityMessageService>>(), CoreOptionsMock, _serializer, MessageBusAdapterMock.Object);
     }
 
     [Fact]
@@ -23,9 +47,14 @@ public class ActivityMessageServiceTests : WithMocks, IClassFixture<JsonLdSerial
         Assert.NotNull(_service);
     }
 
-    [Fact]
+    [Fact(DisplayName = "Should send a message to the delivery queue")]
     public void CanDeliver()
     {
-        Assert.Fail("todo");
+        _subject.Subscribe(c =>
+        {
+            var type = c.Type!.Split(".").Last();
+            Assert.Equal("AnnounceActivity", type);
+        });
+        _service.Deliver(_inbox, _activity, _profile);
     }
 }
