93590143ffacd7d148a7598d854b55129cfb2e02
Joel Mueller <joel.mueller@gmail.com>
2024-01-04 17:10:15 -0600

    Reducing allocations in ShortId generation and parsing

--- Letterbook.Core/Extensions/ShortId.cs
+++ Letterbook.Core/Extensions/ShortId.cs
@@ -6,14 +6,46 @@ public static class ShortId
 {
     public static string ToShortId(this Guid g)
     {
-        var id = Convert.ToBase64String(g.ToByteArray());
-        return id.Replace("=", "").Replace("+", "-").Replace("/", "_");
+        Span<byte> guidBytes = stackalloc byte[16];
+        if (!g.TryWriteBytes(guidBytes))
+        {
+            // this should never happen
+            throw new InvalidOperationException("Could not write Guid bytes");
+        }
+
+        Span<char> base64 = stackalloc char[24];
+        if (!Convert.TryToBase64Chars(guidBytes, base64, out int _))
+        {
+            // this should never happen
+            throw new InvalidOperationException("Could not convert to base64");
+        }
+
+        base64.Replace('+', '-');
+        base64.Replace('/', '_');
+
+        return new string(base64.TrimEnd('='));
     }
 
-    public static Guid ToGuid(string shortId)
+    public static Guid ToGuid(ReadOnlySpan<char> shortId)
     {
-        var id = shortId.Replace("-", "+").Replace("_", "/").PadRight(24, '=');
-        return new Guid(Convert.FromBase64String(id));
+        if (shortId.Length != 22)
+        {
+            throw new ArgumentException("Invalid shortId", nameof(shortId));
+        }
+
+        Span<char> id = stackalloc char[24];
+        shortId.CopyTo(id);
+        id.Replace('-', '+');
+        id.Replace('_', '/');
+        id[^2..].Fill('=');
+
+        Span<byte> guidBytes = stackalloc byte[16];
+        if (!Convert.TryFromBase64Chars(id, guidBytes, out int _))
+        {
+            throw new ArgumentException("Invalid shortId", nameof(shortId));
+        }
+
+        return new Guid(guidBytes);
     }
 
     public static string NewShortId()
