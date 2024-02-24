07e5352382779fa9910e1d6826b7ac65952e721e
Jenniferplusplus <contact@jenniferplusplus.com>
2024-02-23 21:15:43 -0600

    A few more tests

--- Letterbook.Adapter.Db.Tests/RelationalContextTest.cs
+++ Letterbook.Adapter.Db.Tests/RelationalContextTest.cs
@@ -1,11 +1,15 @@
+using System.Diagnostics;
+using Letterbook.Core.Tests;
 using Letterbook.Core.Tests.Fakes;
+using Letterbook.Core.Values;
 using Medo;
 using Microsoft.Extensions.Options;
 
 namespace Letterbook.Adapter.Db.Tests;
 
-public class RelationalContextTest
+public class RelationalContextTest : IDisposable
 {
+    public void Dispose() => _context.Dispose();
     private readonly RelationalContext _context;
 
     public RelationalContextTest()
@@ -22,11 +26,26 @@ public class RelationalContextTest
     }
     
     [Fact]
-    public void CanAddEntity()
+    public void CanTrackAccounts()
     {
         var account = new FakeAccount().Generate();
-        // _context.Add(account);
         _context.Add(account);
     }
 
+    [Fact]
+    public void CanTrackProfiles()
+    {
+        var profile = new FakeProfile().Generate();
+        _context.Add(profile);
+    }
+
+    [Fact]
+    public void CanTrackFollows()
+    {
+        var profile = new FakeProfile("letterbook.example").Generate();
+        var follower = new FakeProfile().Generate();
+        profile.AddFollower(follower, FollowState.Accepted);
+        
+        _context.Add(profile);
+    }
 }
