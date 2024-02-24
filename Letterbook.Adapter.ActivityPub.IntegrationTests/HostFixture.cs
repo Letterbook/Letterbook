d87936a2f73a6478f908749b85d17b9815c62fec
Jenniferplusplus <contact@jenniferplusplus.com>
2024-02-24 00:58:21 -0600

    missed one

--- Letterbook.Adapter.ActivityPub.IntegrationTests/HostFixture.cs
+++ Letterbook.Adapter.ActivityPub.IntegrationTests/HostFixture.cs
@@ -36,7 +36,7 @@ public class HostFixture : WebApplicationFactory<Program>
     /// <param name="builder"></param>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.UseEnvironment("Integration");
+        builder.UseEnvironment("Sandcastle");
         builder.ConfigureTestServices(services =>
         {
             if (services.SingleOrDefault(descriptor => descriptor.ServiceType == typeof(IAccountProfileAdapter)) is { } adapter)
