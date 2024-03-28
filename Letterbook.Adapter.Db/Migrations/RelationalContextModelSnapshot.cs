f4ac98c768ef0ce51b966bb14ce94225586ad7fc
Jenniferplusplus <contact@jenniferplusplus.com>
2024-03-27 23:19:50 -0500

    Revert "Migrate content sort keys"
    This reverts commit e01217cf18cd9c3b92308cef1d4f0bdb8819ec29.

--- Letterbook.Adapter.Db/Migrations/RelationalContextModelSnapshot.cs
+++ Letterbook.Adapter.Db/Migrations/RelationalContextModelSnapshot.cs
@@ -132,9 +132,6 @@ namespace Letterbook.Adapter.Db.Migrations
                     b.Property<string>("Preview")
                         .HasColumnType("text");
 
-                    b.Property<int?>("SortKey")
-                        .HasColumnType("integer");
-
                     b.Property<string>("Source")
                         .HasColumnType("text");
 
