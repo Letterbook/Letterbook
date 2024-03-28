f9db1211675f4ad3b3998f4719cca9bbaf2b36a5
Jenniferplusplus <contact@jenniferplusplus.com>
2024-03-27 22:53:10 -0500

    Configure RelationalContext ahead of time
    This saves EFCore from having to create large numbers of DI containers, which can impact performance

--- Letterbook.Adapter.Db/RelationalContext.cs
+++ Letterbook.Adapter.Db/RelationalContext.cs
@@ -15,14 +15,13 @@ namespace Letterbook.Adapter.Db;
 /// <summary>
 /// This is the DbContext for most of the application. The actual data records (like the contents of profiles and
 /// posts) will be managed here.
-/// 
+///
 /// Feeds/timelines will likely be managed separately from this, likely using a timeseries db, probably timescale.
 /// Maybe notifications, too.
 /// There may be a need for search and/or graph databases in the future, and those would also be separate from this.
 /// </summary>
 public class RelationalContext : DbContext
 {
-    private readonly DbOptions _config;
     public DbSet<Note> Notes { get; set; }
     public DbSet<Profile> Profiles { get; set; }
     public DbSet<Account> Accounts { get; set; }
@@ -32,21 +31,11 @@ public class RelationalContext : DbContext
     // Called by the designer to create and run migrations
     internal RelationalContext(DbContextOptions<RelationalContext> context) : base(context)
     {
-        _config = new DesignDbOptions();
     }
 
     // Called by DI for normal use
-    public RelationalContext(IOptions<DbOptions> config)
+    public RelationalContext()
     {
-        _config = config.Value;
-    }
-
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        var builder = new NpgsqlDataSourceBuilder(_config.GetConnectionString());
-        builder.EnableDynamicJson();
-        options.UseNpgsql(builder.Build());
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -54,7 +43,7 @@ public class RelationalContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
-    
+
     protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
     {
         configurationBuilder.Properties<Uri>().HaveConversion<UriIdConverter, UriIdComparer>();
