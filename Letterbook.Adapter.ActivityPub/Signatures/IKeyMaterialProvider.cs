0e2164492c0294edfd54fb4885b75ba96154a1f4
Lauri Kotilainen <lauri.kotilainen@devisioona.fi>
2024-04-05 23:34:15 +0300

    Added some wiring for HTTP signature authentication

--- Letterbook.Adapter.ActivityPub/Signatures/IKeyMaterialProvider.cs
+++ Letterbook.Adapter.ActivityPub/Signatures/IKeyMaterialProvider.cs
@@ -4,5 +4,13 @@ namespace Letterbook.Adapter.ActivityPub.Signatures;
 
 public interface IKeyMaterialProvider
 {
-	Task<X509Certificate2?> GetKeyByIdAsync(string? signatureParamsKeyId);
+	Task<X509Certificate2?> GetKeyByIdAsync(string? signatureParamsKeyId, CancellationToken cancellationToken = default);
+}
+
+public class HttpClientKeyMaterialProvider : IKeyMaterialProvider
+{
+	public Task<X509Certificate2?> GetKeyByIdAsync(string? signatureParamsKeyId, CancellationToken cancellationToken = default)
+	{
+		return Task.FromResult<X509Certificate2?>(null);
+	}
 }
---
+++ Letterbook.Api.Authentication.HttpSignature/HttpSignatureAuthenticationHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Encodings.Web;
+using Letterbook.Adapter.ActivityPub.Signatures;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Letterbook.Api.Authentication.HttpSignature;
+
+public class HttpSignatureAuthenticationHandler : AuthenticationHandler<HttpSignatureAuthenticationOptions>
+{
+	private readonly IFederatedActorHttpSignatureVerifier _signatureVerifier;
+
+	public HttpSignatureAuthenticationHandler(
+		IOptionsMonitor<HttpSignatureAuthenticationOptions> options,
+		ILoggerFactory logger,
+		IFederatedActorHttpSignatureVerifier signatureVerifier,
+		UrlEncoder encoder)
+		: base(options, logger, encoder)
+	{
+		_signatureVerifier = signatureVerifier;
+	}
+
+	protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+	{
+		var isValid = await _signatureVerifier.VerifyAsync(Context, Context.RequestAborted);
+
+		return AuthenticateResult.NoResult();
+	}
+}
+
+public class HttpSignatureAuthenticationOptions : AuthenticationSchemeOptions
+{
+}
+
+public static class HttpSignatureAuthenticationDefaults
+{
+	public static readonly string Scheme = "HttpSignature";
+}
+
+public static class HttpSignatureAuthenticationExtensions
+{
+	public static AuthenticationBuilder AddHttpSignature(this AuthenticationBuilder builder)
+	{
+		return builder.AddScheme<HttpSignatureAuthenticationOptions, HttpSignatureAuthenticationHandler>(
+			HttpSignatureAuthenticationDefaults.Scheme,
+			static _ =>
+			{
+
+			});
+	}
+}
