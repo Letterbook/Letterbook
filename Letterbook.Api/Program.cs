4a1078bb7bb6481a6392a0610aed6eaa1c14d980
Jenniferplusplus <contact@jenniferplusplus.com>
2024-04-10 21:31:11 -0500

    Extract most DI config to be reusable

--- Letterbook.Api/Program.cs
+++ Letterbook.Api/Program.cs
@@ -12,6 +12,7 @@ using Letterbook.Api.Swagger;
 using Letterbook.Core;
 using Letterbook.Core.Adapters;
 using Letterbook.Core.Authorization;
+using Letterbook.Core.Exceptions;
 using Letterbook.Core.Extensions;
 using Letterbook.Core.Models;
 using Letterbook.Core.Workers;
@@ -43,9 +44,8 @@ public class Program
 			.CreateBootstrapLogger();
 
 		var builder = WebApplication.CreateBuilder(args);
-		var coreSection = builder.Configuration.GetSection(CoreOptions.ConfigKey);
-		var coreOptions = coreSection.Get<CoreOptions>()
-						  ?? throw new ArgumentException("Invalid configuration", nameof(CoreOptions));
+		var coreOptions = builder.Configuration.GetSection(CoreOptions.ConfigKey).Get<CoreOptions>()
+		                  ?? throw new ConfigException(nameof(CoreOptions));
 
 		if (!builder.Environment.IsProduction())
 			builder.Configuration.AddUserSecrets<Program>();
@@ -57,116 +57,15 @@ public class Program
 			.ReadFrom.Services(services)
 		);
 
-		// Register controllers
-		builder.Services
-			.AddControllers(options =>
-			{
-				options.ModelBinderProviders.Insert(0, new Uuid7BinderProvider());
-				options.Conventions.Add(new RouteTokenTransformerConvention(new SnakeCaseRouteTransformer()));
-				options.OutputFormatters.Insert(0, new JsonLdOutputFormatter());
-				options.InputFormatters.Insert(0, new JsonLdInputFormatter());
-			})
-			.AddJsonOptions(options =>
-			{
-				options.JsonSerializerOptions.Converters.Add(new Json.Uuid7JsonConverter());
-				options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-			})
-			.Services.Configure<ApiBehaviorOptions>(options =>
-			{
-				options.InvalidModelStateResponseFactory = context =>
-				{
-					var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
-					var problemDetails = new ValidationProblemDetails(context.ModelState)
-					{
-						Instance = context.HttpContext.Request.Path,
-						Status = (int)HttpStatusCode.BadRequest,
-					};
-					logger.LogInformation("Validation failed {@Problem}", problemDetails);
-
-					return new BadRequestObjectResult(problemDetails);
-				};
-			});
-		builder.Services.AddWebfinger();
-
-		// Register Authentication
-		builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-			.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-			{
-				options.Authority = coreOptions.BaseUri().ToString();
-				options.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateAudience = false
-				};
-				// TODO(Security): Figure out how to do this only in Development
-				options.RequireHttpsMetadata = false;
-			});
-
+		builder.Services.AddApiProperties(builder.Configuration);
 		// Register Open Telemetry
-		builder.Services.AddOpenTelemetry()
-			.ConfigureResource(resource =>
-			{
-				resource.AddService("Letterbook");
-			})
-			.WithMetrics(metrics =>
-			{
-				metrics.AddAspNetCoreInstrumentation();
-				metrics.AddHttpClientInstrumentation();
-				metrics.AddMeter("Npgsql");
-				metrics.AddPrometheusExporter();
-			})
-			.WithTracing(tracing =>
-			{
-				tracing.AddAspNetCoreInstrumentation();
-				tracing.AddHttpClientInstrumentation();
-				tracing.AddNpgsql();
-				tracing.AddOtlpExporter();
-			});
+		builder.Services.AddTelemetry();
 		builder.Services.AddHealthChecks();
-
-		builder.Services.AddActivityPubClient(coreOptions.DomainName);
-
-		// Register options
-		builder.Services.Configure<CoreOptions>(coreSection);
-		builder.Services.Configure<ApiOptions>(builder.Configuration.GetSection(ApiOptions.ConfigKey));
-		builder.Services.Configure<DbOptions>(builder.Configuration.GetSection(DbOptions.ConfigKey));
-
-		// Register Mapping Configs
-		builder.Services.AddSingleton<MappingConfigProvider>();
-
-		// Register Services
-		builder.Services.AddScoped<IActivityEventService, ActivityEventService>();
-		builder.Services.AddScoped<IProfileEventService, ProfileEventService>();
-		builder.Services.AddScoped<IAccountService, AccountService>();
-		builder.Services.AddScoped<IProfileService, ProfileService>();
-		builder.Services.AddScoped<IPostService, PostService>();
-		builder.Services.AddScoped<IAccountEventService, AccountEventService>();
-		builder.Services.AddScoped<IAccountProfileAdapter, AccountProfileAdapter>();
-		builder.Services.AddScoped<IActivityMessageService, ActivityMessageService>();
-		builder.Services.AddScoped<IAuthzPostService, PostService>();
-		builder.Services.AddScoped<IPostEventService, PostEventService>();
-		builder.Services.AddSingleton<IAuthorizationService, AuthorizationService>();
-
-		// Register Workers
-		builder.Services.AddScoped<SeedAdminWorker>();
-		builder.Services.AddScoped<DeliveryWorker>();
-		builder.Services.AddSingleton<DeliveryObserver>();
-		builder.Services.AddHostedService<WorkerScope<SeedAdminWorker>>();
-		builder.Services.AddHostedService<MessageWorkerHost<DeliveryObserver, ASType>>((DeliveryObserverFactory));
-
-		// Register Adapters
-		builder.Services.AddScoped<IActivityAdapter, ActivityAdapter>();
-		builder.Services.AddScoped<IPostAdapter, PostAdapter>();
-		builder.Services.AddRxMessageBus();
-		builder.Services.AddSingleton<IActivityPubDocument, Document>();
-		builder.Services.AddDbAdapter(builder.Configuration.GetSection(DbOptions.ConfigKey));
-		builder.Services.AddDbContext<FeedsContext>();
-		builder.Services.AddIdentity<Account, IdentityRole<Guid>>()
-			.AddEntityFrameworkStores<RelationalContext>();
-		builder.Services.TryAddTypesModule();
-
-		builder.Services.ConfigureSwagger();
+		builder.Services.AddActivityPubClient(builder.Configuration);
+		builder.Services.AddServices(builder.Configuration);
 
 		builder.WebHost.UseUrls(coreOptions.BaseUri().ToString());
+
 		var app = builder.Build();
 		// Configure the HTTP request pipeline.
 
@@ -187,26 +86,13 @@ public class Program
 		app.UseAuthorization();
 
 		app.UseHealthChecks("/healthz");
-
-		app.UseWebFingerScoped();
-
 		app.MapPrometheusScrapingEndpoint();
-
 		app.UseSerilogRequestLogging();
 
+		app.UseWebFingerScoped();
 		app.UsePathBase(new PathString("/api/v1"));
 		app.MapControllers();
 
 		app.Run("http://localhost:5127");
-	}
-
-	private static MessageWorkerHost<DeliveryObserver, ASType> DeliveryObserverFactory(IServiceProvider provider)
-	{
-		// Set a 50ms delay on the delivery observer
-		// This is just a guess at a sufficient amount of time for peers to become ready to accept reply messages
-		// We don't want to sit on them for too long, because they'll just sitting in RAM
-		return new MessageWorkerHost<DeliveryObserver, ASType>(
-			provider.GetRequiredService<ILogger<MessageWorkerHost<DeliveryObserver, ASType>>>(), provider,
-			provider.GetRequiredService<IMessageBusClient>(), 50);
 	}
 }
