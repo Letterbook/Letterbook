84710e636a23bab92f102daffd63abf6d3f35e70
Matthew Lyon <matthew@lyonheart.us>
2024-04-16 18:31:45 -0700

    cleanup, add domain name to handle

--- Letterbook.Web/Pages/Profile.cshtml.cs
+++ Letterbook.Web/Pages/Profile.cshtml.cs
@@ -2,27 +2,31 @@
 using Models = Letterbook.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Html;
+using Microsoft.Extensions.Options;
 
 namespace Letterbook.Web.Pages;
 
 public class Profile : PageModel
 {
 	private readonly IProfileService _profiles;
+	private readonly CoreOptions _options;
 	
 	public required string Handle { get; set; }
 	public required string DisplayName { get; set; }
-	public required string Description { get; set; }
+	public required HtmlString Description { get; set; }
 	public required Models.CustomField[] CustomFields { get; set; }
 	
 	private protected Models.Profile? Prof { get; set; }
 	
-	public int GetFollowerCount => Prof!.FollowersCollection.Count;
-	public int GetFollowingCount => Prof!.FollowingCollection.Count;
+	public int GetFollowerCount() => Prof!.FollowersCollection.Count;
+	public int GetFollowingCount() => Prof!.FollowingCollection.Count;
 
 
-	public Profile(IProfileService profiles)
+	public Profile(IProfileService profiles, IOptions<CoreOptions> options)
 	{
 		_profiles = profiles;
+		_options = options.Value;
 	}
 
 	public async Task<IActionResult> OnGet(string handle)
@@ -31,9 +35,10 @@ public class Profile : PageModel
 		if (found.FirstOrDefault() is not { } profile)
 			return NotFound();
 		Prof = profile;
-		Handle = handle;
+		
+		Handle = $"@{handle}@{_options.DomainName}";
 		DisplayName = profile.DisplayName;
-		Description = profile.Description;
+		Description = new HtmlString(profile.Description);
 		CustomFields = profile.CustomFields;
 
 
