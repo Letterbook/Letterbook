952f498674096ea29616649dbb98b8239044b1f5
Jenniferplusplus <contact@jenniferplusplus.com>
2024-05-03 01:11:17 -0500

    Emit post events

--- Letterbook.Core/IPostEventService.cs
+++ Letterbook.Core/IPostEventService.cs
@@ -1,5 +1,4 @@
 using Letterbook.Core.Models;
-using Microsoft.Extensions.Logging;
 
 namespace Letterbook.Core;
 
@@ -13,49 +12,4 @@ public interface IPostEventService
 	public void Liked(Post post, Profile actor);
 	public void Shared(Post post, Profile actor);
 
-}
-
-public class PostEventService : IPostEventService
-{
-	private readonly ILogger<PostEventService> _logger;
-
-	public PostEventService(ILogger<PostEventService> logger)
-	{
-		_logger = logger;
-	}
-
-	public void Created(Post post)
-	{
-		_logger.LogWarning($"{nameof(Created)} event not implemented");
-	}
-
-	public void Deleted(Post post)
-	{
-		_logger.LogWarning($"{nameof(Deleted)} event not implemented");
-	}
-
-	public void Updated(Post post)
-	{
-		_logger.LogWarning($"{nameof(Updated)} event not implemented");
-	}
-
-	public void Published(Post post)
-	{
-		_logger.LogWarning($"{nameof(Published)} event not implemented");
-	}
-
-	public void Received(Post post, Profile actor)
-	{
-		_logger.LogWarning($"{nameof(Received)} event not implemented");
-	}
-
-	public void Liked(Post post, Profile actor)
-	{
-		_logger.LogWarning($"{nameof(Liked)} event not implemented");
-	}
-
-	public void Shared(Post post, Profile actor)
-	{
-		_logger.LogWarning($"{nameof(Shared)} event not implemented");
-	}
 }
---
+++ Letterbook.Core/PostEventService.cs
@@ -0,0 +1,79 @@
+﻿using CloudNative.CloudEvents;
+using Letterbook.Core.Adapters;
+using Letterbook.Core.Extensions;
+using Letterbook.Core.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Letterbook.Core;
+
+public class PostEventService : IPostEventService
+{
+	private readonly ILogger<PostEventService> _logger;
+	private readonly CoreOptions _options;
+	private IObserver<CloudEvent> _channel;
+
+	public PostEventService(ILogger<PostEventService> logger, IOptions<CoreOptions> options, IMessageBusAdapter messageBusAdapter)
+	{
+		_logger = logger;
+		_options = options.Value;
+		_channel = messageBusAdapter.OpenChannel<Post>(nameof(PostEventService));
+	}
+
+	public void Created(Post post)
+	{
+		var message = FormatMessage(post, nameof(Created));
+		_channel.OnNext(message);
+	}
+
+	public void Deleted(Post post)
+	{
+		var message = FormatMessage(post, nameof(Deleted));
+		_channel.OnNext(message);
+	}
+
+	public void Updated(Post post)
+	{
+		var message = FormatMessage(post, nameof(Updated));
+		_channel.OnNext(message);
+	}
+
+	public void Published(Post post)
+	{
+		var message = FormatMessage(post, nameof(Published));
+		_channel.OnNext(message);
+	}
+
+	public void Received(Post post, Profile actor)
+	{
+		var message = FormatMessage(post, nameof(Received));
+		_channel.OnNext(message);
+	}
+
+	public void Liked(Post post, Profile actor)
+	{
+		var message = FormatMessage(post, nameof(Liked));
+		_channel.OnNext(message);
+	}
+
+	public void Shared(Post post, Profile actor)
+	{
+		var message = FormatMessage(post, nameof(Shared));
+		_channel.OnNext(message);
+	}
+
+	private IObserver<CloudEvent> GetChannel() => _channel;
+
+	private CloudEvent FormatMessage(Post value, string action)
+	{
+		return new CloudEvent
+		{
+			Id = Guid.NewGuid().ToString(),
+			Source = _options.BaseUri(),
+			Data = value,
+			Type = $"{nameof(Post)}.{action}",
+			Subject = value.Id.ToString(),
+			Time = DateTimeOffset.UtcNow
+		};
+	}
+}
