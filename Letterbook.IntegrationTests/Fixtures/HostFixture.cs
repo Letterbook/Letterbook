5c0ddaac20804b9809aa47cf73c542d7932329f3
Jenniferplusplus <contact@jenniferplusplus.com>
2024-06-06 00:24:55 -0500

    Initialize feeds test data

--- Letterbook.IntegrationTests/Fixtures/HostFixture.cs
+++ Letterbook.IntegrationTests/Fixtures/HostFixture.cs
@@ -1,9 +1,11 @@
+using Bogus;
 using Letterbook.Adapter.Db;
 using Letterbook.Adapter.TimescaleFeeds;
 using Letterbook.Core;
+using Letterbook.Core.Extensions;
 using Letterbook.Core.Models;
 using Letterbook.Core.Tests.Fakes;
-using Letterbook.Core.Tests.Fixtures;
+using Letterbook.Core.Values;
 using Letterbook.Core.Workers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -17,8 +19,8 @@ using RelationalContext = Letterbook.Adapter.Db.RelationalContext;
 
 namespace Letterbook.IntegrationTests.Fixtures;
 
-public class HostFixture<T> : WebApplicationFactory<Program>, IIntegrationTestData
-where T : ITestSeed
+public class HostFixture<T> : WebApplicationFactory<Program>
+	where T : ITestSeed
 {
 	protected override void Dispose(bool disposing)
 	{
@@ -42,6 +44,7 @@ where T : ITestSeed
 		"SSL Mode=Disable;" +
 		"Search Path=public;" +
 		"Include Error Detail=true";
+
 	private string FeedsConnectionString =
 		$"Database=letterbook_feeds_{typeof(T).Name};" +
 		"Server=localhost;" +
@@ -100,23 +103,19 @@ where T : ITestSeed
 
 	private void InitializeTestData()
 	{
-		lock (_lock)
-		{
+		_sink.OnMessage(new DiagnosticMessage("Bogus Seed: {0}", Init.WithSeed(T.Seed())));
+		InitTestData();
+		InitTimelineData();
 
-			_sink.OnMessage(new DiagnosticMessage("Bogus Seed: {0}", Init.WithSeed(T.Seed())));
-			this.InitTestData(Options);
-			// this.InitTimelineData(Options);
-
-			Deleted = _context.Database.EnsureDeleted();
-			_context.Database.Migrate();
-			_context.Accounts.AddRange(Accounts);
-			_context.Profiles.AddRange(Profiles);
-			Records = _context.SaveChanges();
-			_context.Posts.AddRange(Posts.SelectMany(pair => pair.Value));
-			Records += _context.SaveChanges();
-			// context.Posts.AddRange(Timeline);
-			// Records += context.SaveChanges();
-		}
+		Deleted = _context.Database.EnsureDeleted();
+		_context.Database.Migrate();
+		_context.Accounts.AddRange(Accounts);
+		_context.Profiles.AddRange(Profiles);
+		Records = _context.SaveChanges();
+		_context.Posts.AddRange(Posts.SelectMany(pair => pair.Value));
+		Records += _context.SaveChanges();
+		_context.Posts.AddRange(Timeline);
+		Records += _context.SaveChanges();
 	}
 
 	protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -145,6 +144,116 @@ where T : ITestSeed
 			});
 
 		base.ConfigureWebHost(builder);
+	}
+
+	private void InitTestData()
+	{
+		var authority = Options.BaseUri() ?? new Uri("letterbook.example");
+		Accounts.AddRange(new FakeAccount(false).Generate(2));
+		Profiles.AddRange(new FakeProfile(authority, Accounts[0]).Generate(3));
+		Profiles.Add(new FakeProfile(authority, Accounts[1]).Generate());
+		Profiles.AddRange(new FakeProfile().Generate(3));
+
+		// P0 follows P4 and P5
+		// P4 follows P0
+		Profiles[0].Follow(Profiles[4], FollowState.Accepted);
+		Profiles[0].Follow(Profiles[5], FollowState.Accepted);
+		Profiles[0].AddFollower(Profiles[4], FollowState.Accepted);
+
+		// Local profiles
+		// P0 creates posts 0-2
+		Posts.Add(Profiles[0], new FakePost(Profiles[0]).Generate(3));
+		// P0 creates post 3, as reply to 2
+		Posts[Profiles[0]].Add(new FakePost(Profiles[0], Posts[Profiles[0]][2]).Generate());
+		// P1 creates posts 0-7
+		Posts.Add(Profiles[1], new FakePost(Profiles[0], opts: Options).Generate(8));
+		// P2 creates post 0, and draft 1
+		Posts.Add(Profiles[2], new FakePost(Profiles[2]).Generate(1));
+		Posts[Profiles[2]].Add(new FakePost(Profiles[2], draft: true).Generate());
+
+		// Remote profiles
+		// P4 creates post 0, as reply to post P0:3
+		Posts.Add(Profiles[4], new FakePost(Profiles[3], Posts[Profiles[0]][3]).Generate(1));
+
+		var allAudience = Posts
+			.SelectMany(pair => pair.Value)
+			.SelectMany(post => post.Audience)
+			.ToHashSet();
+
+		foreach (var post in Posts.SelectMany(pair => pair.Value))
+		{
+			var set = new HashSet<Audience>();
+			foreach (var audience in post.Audience)
+			{
+				set.Add(allAudience.TryGetValue(audience, out var existing) ? existing : audience);
+			}
+
+			post.Audience = set;
+		}
+	}
+
+	private void InitTimelineData()
+	{
+		var authority = Options.BaseUri() ?? new Uri("letterbook.example");
+		var faker = new Faker();
+
+		while (Timeline.Count < 1000)
+		{
+			Profile creator;
+			// Pick or add new Creator
+			if (Profiles.Count > 0 && faker.Random.Bool(0.9f))
+			{
+				creator = faker.PickRandom(Profiles);
+			}
+			else
+			{
+				if (faker.Random.Bool(0.3f))
+				{
+					creator = new FakeProfile(authority).Generate();
+					Profiles.Add(creator);
+				}
+				else
+				{
+					creator = new FakeProfile().Generate();
+					Profiles.Add(creator);
+				}
+			}
+
+			Timeline.AddRange(GeneratePosts(creator));
+		}
+
+		var allAudience = Posts
+			.SelectMany(pair => pair.Value)
+			.SelectMany(post => post.Audience)
+			.ToHashSet();
+		allAudience.UnionWith(Timeline.SelectMany(post => post.Audience));
+
+		foreach (var post in Timeline)
+		{
+			var set = new HashSet<Audience>(allAudience);
+			set.IntersectWith(post.Audience);
+
+			post.Audience = set;
+		}
+
+		return;
+
+		IEnumerable<Post> GeneratePosts(Profile creator)
+		{
+			Post? inReplyTo = default;
+			if (Timeline.Count != 0 && faker.Random.Bool(0.6f))
+				inReplyTo = faker.PickRandom(Timeline);
+
+			var fake = inReplyTo == null
+				? new FakePost(creator)
+				: new FakePost(creator, inReplyTo);
+
+			var count = inReplyTo == null
+				? faker.Random.Number(1, 3)
+				: faker.Random.Number(1, 9);
+
+			return fake.Generate(count);
+		}
 	}
 }
 
