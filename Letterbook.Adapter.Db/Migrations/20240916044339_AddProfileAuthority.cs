902cc0922e844570e2de2c69f647340c6f769722
Jenniferplusplus <contact@jenniferplusplus.com>
2024-09-26 01:02:52 -0500

    migrate values for Authority column

--- Letterbook.Adapter.Db/Migrations/20240916044339_AddProfileAuthority.cs
+++ Letterbook.Adapter.Db/Migrations/20240916044339_AddProfileAuthority.cs
@@ -22,26 +22,36 @@ namespace Letterbook.Adapter.Db.Migrations
                 table: "Profiles",
                 column: "Authority");
 
+            migrationBuilder.Sql(
+				"""
+				CREATE OR REPLACE FUNCTION array_reverse(anyarray) RETURNS anyarray AS $$
+				SELECT ARRAY(
+					SELECT $1[i]
+					FROM generate_subscripts($1,1) AS s(i)
+					ORDER BY i DESC
+				);
+				$$ LANGUAGE 'sql' STRICT IMMUTABLE;
+				""");
+
             /**
              * regex:
              * \w+:\/\/				the scheme (ex. https://)
              * (.*@)?				discarded capture group for the user info, just in case
-             * (\w+\.\w+[\.\w+]*)	capture group for the actual host name, which is what we want
+             * (\w+[\.\w+]*)		capture group for the actual host name, which is what we want
              * \/?.*				the port, path, query, and fragment portions
              */
-            // \w+:\/\/(.*@)?(\w+\.\w+[\.\w+]*)\/?.*
-
-            // We have no servers, so no valuable data. So, we can probably skip this if necessary.
-            // But, this migration would actually leave existing profiles in a bad state.
-            // What we would need to do is extract, tokenize, reverse, and recombine the domain from the profile's
-            // fediId as part of the migration.
-            // Which is a _challenge_ to do in SQL.
-            // migrationBuilder.Sql(
-	            // """
-	            // UPDATE "Profiles" p
-	            // SET Authority=(SELECT "FediId" FROM "Profiles" p2 WHERE p2."Id"=p."Id")
-	            // """
-            // );
+            // \w+:\/\/(.*@)?(\w+[\.\w+]*)\/?.*
+            migrationBuilder.Sql(
+				"""
+				UPDATE "Profiles" p
+				SET "Authority"=(
+					SELECT array_to_string(array_reverse(string_to_array(regexp_replace(p2."FediId", '\w+:\/\/(.*@)?(\w+[\.\w+]*)\/?.*', '\2'), '.')), '.')
+					FROM "Profiles" p2
+					WHERE p2."Id"=p."Id"
+				)
+				WHERE p."Authority" = '';
+				"""
+            );
         }
 
         /// <inheritdoc />
