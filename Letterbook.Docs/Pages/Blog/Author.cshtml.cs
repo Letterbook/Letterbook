7178ebfcae885dec7e7fe17dc62b19d57f62ee3f
Jenniferplusplus <contact@jenniferplusplus.com>
2024-12-10 00:24:42 -0600

    author pages

---
+++ Letterbook.Docs/Pages/Blog/Author.cshtml.cs
@@ -0,0 +1,18 @@
+using Letterbook.Docs.Markdown;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Letterbook.Docs.Pages.Blog;
+
+public class Author([FromServices] LoaderFactory loader) : PageModel
+{
+	public LoadDate Blog { get; set; } = loader.LoadFrom<LoadDate, MarkdownDoc>("_blog");
+	public LoadDirectory Authors { get; set; } = loader.LoadFrom<LoadDirectory, MarkdownAuthor>("_authors");
+
+	[FromRoute]
+	public string Slug { get; set; }
+
+	public MarkdownAuthor? Doc => Authors.GetBySlug<MarkdownAuthor>(Slug);
+
+	public IEnumerable<MarkdownDoc> Articles => Doc != null ? Blog.GetAll<MarkdownDoc>().Where(d => d.Authors.Contains(Doc.Id)) : [];
+}
---
+++ Letterbook.Docs/_authors/jenniferplusplus.md
@@ -0,0 +1,17 @@
+---
+id: jenniferplusplus
+displayName: Jennifer Moore
+url: https://jenniferplusplus.com
+socials:
+  - label: Fediverse
+    value: https://hachyderm.io/@jenniferplusplus
+  - label: Bluesky
+    value: https://bsky.app/profile/jenniferplusplus.com
+image:
+    href: /img/authors/jennierplusplus.jpg
+    alt: Selfie of Jennifer, taken outdoors. She's a white woman with glasses and blue-purple hair.
+---
+
+# Jennifer Moore
+
+Jennifer is the primary maintainer of Letterbook. She's worked in software development since 2009, and is more online than is entirely healthy.
