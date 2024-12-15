797deadc5a8c108695863a6274de5983663eab30
Jenniferplusplus <contact@jenniferplusplus.com>
2024-12-14 22:09:35 -0600

    simplify mermaid init
    it won't reinitialize, so there's no point trying to handle theme changes after load

--- Letterbook.Docs/wwwroot/js/mermaid-config.js
+++ Letterbook.Docs/wwwroot/js/mermaid-config.js
@@ -2,15 +2,6 @@
 function isDarkMode() {
     return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
-
-function updateTheme() {
-    mermaid.initialize({
-        theme: isDarkMode() ? 'dark' : 'default',
-    });
-}
-
-// Initial theme setup
-updateTheme();
-
-// Listen for changes in the color scheme preference
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
+mermaid.initialize({
+    theme: isDarkMode() ? 'dark' : 'default',
+});
