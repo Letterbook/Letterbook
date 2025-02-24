85bcf76589760bc4afc7cf3205a6ba2cee7eefd1
Jenniferplusplus <contact@jenniferplusplus.com>
2025-02-23 22:18:24 -0600

    Seed system profiles

--- Letterbook.Core/Workers/SeedAdminWorker.cs
+++ Letterbook.Core/Workers/SeedAdminWorker.cs
@@ -1,29 +1,28 @@
 using Letterbook.Core.Adapters;
-using Letterbook.Core.Extensions;
-using Microsoft.Extensions.DependencyInjection;
+using Letterbook.Core.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Letterbook.Core.Workers;
 
 /// <summary>
-/// This should eventually seed the admin invite on first launch. For now it just creates an account, as a convenience
-/// during development
+/// Seeds the database with system profiles
 /// </summary>
 public class SeedAdminWorker : IScopedWorker
 {
 	private readonly ILogger<SeedAdminWorker> _logger;
 	private readonly CoreOptions _coreOptions;
 	private readonly IAccountService _accountService;
-	private readonly IDataAdapter _accountAdapter;
+	private readonly IDataAdapter _data;
 
 	public SeedAdminWorker(ILogger<SeedAdminWorker> logger, IOptions<CoreOptions> coreOpts,
-		IAccountService accountService, IDataAdapter accountAdapter)
+		IAccountService accountService, IDataAdapter data)
 	{
 		_logger = logger;
 		_coreOptions = coreOpts.Value;
 		_accountService = accountService;
-		_accountAdapter = accountAdapter;
+		_data = data;
 	}
 
 	public async Task DoWork(CancellationToken cancellationToken)
@@ -32,12 +31,28 @@ public class SeedAdminWorker : IScopedWorker
 
 		try
 		{
-			if (_accountAdapter.AllAccounts().Any())
+			var instance = Profile.SystemInstance(_coreOptions);
+			if (!await _data.Profiles(instance.Id).AnyAsync(cancellationToken: cancellationToken))
+			{
+				_data.Add(instance);
+				_logger.LogInformation("Seeding instance profile {Id}", instance.Id);
+			}
+
+			var mods = Profile.SystemModerators(_coreOptions);
+			if (!await _data.Profiles(mods.Id).AnyAsync(cancellationToken: cancellationToken))
+			{
+				_data.Add(mods);
+				_logger.LogInformation("Seeding moderator profile {Id}", mods.Id);
+			}
+
+			await _data.Commit();
+
+			// TODO: replace with invite mechanism
+			if (_data.AllAccounts().Any())
 			{
 				_logger.LogDebug("Found accounts, skipping seed");
 				return;
 			}
-
 			var admin = await _accountService.RegisterAccount($"admin@letterbook.example", "admin",
 				"Password1!");
 			if (admin is not null)
