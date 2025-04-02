0d4019ffaf7bf7aafddf790ce14c5c0764290e3f
Jenniferplusplus <contact@jenniferplusplus.com>
2025-04-01 22:56:04 -0500

    icons

--- Source/Letterbook.Web/Pages/Index.cshtml.cs
+++ Source/Letterbook.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Letterbook.Core;
 using Medo;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace Letterbook.Web.Pages;
 
@@ -10,13 +11,15 @@ public class IndexModel : PageModel
 	private readonly IProfileService _profiles;
 	private readonly ILogger<IndexModel> _logger;
 
+	public IOptions<CoreOptions> Opts { get; }
 	public Models.Profile Self { get; set; } = default!;
 	public IAuthzTimelineService Timeline { get; set; } = default!;
 	public IAuthzProfileService Profiles { get; set; } = default!;
 	public string? SelfId => User.Claims.FirstOrDefault(c => c.Type == ApplicationClaims.ActiveProfile)?.Value;
 
-	public IndexModel(ILogger<IndexModel> logger, ITimelineService timeline, IProfileService profiles)
+	public IndexModel(ILogger<IndexModel> logger, IOptions<CoreOptions> opts, ITimelineService timeline, IProfileService profiles)
 	{
+		Opts = opts;
 		_timeline = timeline;
 		_profiles = profiles;
 		_logger = logger;
