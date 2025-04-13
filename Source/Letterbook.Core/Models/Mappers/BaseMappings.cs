68f612a47e1a64348bb5b7b64271ac02e8046cd2
Jenniferplusplus <contact@jenniferplusplus.com>
2025-04-13 00:50:12 -0500

    profileId mappings

--- Source/Letterbook.Core/Models/Mappers/BaseMappings.cs
+++ Source/Letterbook.Core/Models/Mappers/BaseMappings.cs
@@ -76,8 +76,8 @@ public class BaseMappings : AutoMapper.Profile
 		CreateMap<string, Models.Profile>(MemberList.None)
 			.ConstructUsing(s => Models.Profile.CreateEmpty(new ProfileId(ProfileId.FromString(s))));
 
-		CreateMap<Uuid7, ProfileId>();
-		CreateMap<ProfileId, Uuid7>();
+		CreateMap<Uuid7, ProfileId>(MemberList.None).ConstructUsing(id => new(id));
+		CreateMap<ProfileId, Uuid7>(MemberList.None).ConstructUsing(id => id.Id);
 
 
 		CreateMap<DateTimeOffset?, DateTimeOffset?>()
