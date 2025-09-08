4b24690d317badd870303ef25ce44fd512584262
Jenniferplusplus <contact@jenniferplusplus.com>
2025-09-08 00:39:00 -0500

    get account data

--- Source/Letterbook.Web/Pages/Account.cshtml.cs
+++ Source/Letterbook.Web/Pages/Account.cshtml.cs
@@ -1,17 +1,36 @@
+using Letterbook.Core;
+using Letterbook.Core.Extensions;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Letterbook.Web.Pages;
 
-[Authorize]
+[Authorize(Policy = Constants.AuthzPolicy)]
 public class Account : PageModel
 {
-	public Guid AccountId => Guid.Empty;
-	public string DisplayName => "User";
-	public string Email => "user@example.com";
+	private readonly IAccountService _accounts;
 
-	public void OnGet()
+	private Models.Account _self;
+
+	public Guid AccountId => _self.Id;
+	public string DisplayName => _self.Name ?? _self.UserName ?? "unknown";
+	public string Email => _self.Email ?? "";
+	public bool EmailConfirmed => _self.EmailConfirmed;
+
+	public Account(IAccountService accounts)
 	{
-
+		_self = default!;
+		_accounts = accounts;
 	}
+
+	public async Task<IActionResult> OnGet()
+	{
+		if (!User.Claims.TryGetAccountId(out var id) || await _accounts.LookupAccount(id) is not { } account)
+			return Challenge();
+		_self = account;
+
+		return Page();
+	}
+
 }
