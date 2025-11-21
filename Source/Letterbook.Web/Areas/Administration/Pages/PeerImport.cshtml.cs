8ac4cac2b91f46ac5230b9b1bd7ac1c415aaecaa
Jenniferplusplus <contact@jenniferplusplus.com>
2025-11-21 01:15:10 -0600

    form built

--- Source/Letterbook.Web/Areas/Administration/Pages/PeerImport.cshtml.cs
+++ Source/Letterbook.Web/Areas/Administration/Pages/PeerImport.cshtml.cs
@@ -1,3 +1,5 @@
+using Letterbook.Core;
+using Letterbook.Core.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,9 +9,15 @@ public class PeerImport : PageModel
 {
 	[BindProperty] public IFormFile CsvFile { get; set; } = default!;
 	[BindProperty] public bool LetterbookFormat { get; set; }
+	[BindProperty] public ModerationService.MergeStrategy Strategy { get; set; }
 
 	public void OnGet()
 	{
 
+	}
+
+	public string FormateStrategy(string name)
+	{
+		return string.Join(' ', StringFormatters.SplitExp().Split(name));
 	}
 }
