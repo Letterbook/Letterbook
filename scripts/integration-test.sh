d6e6b6e55f75ae6683280fafbd49e38438a72fac
Ben Biddington <ben.biddington@gmail.com>
2026-01-21 14:05:59 +1300

    Allow integration tests to be run as Github workflow

---
+++ scripts/integration-test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+# Test results go to Tests/Letterbook.IntegrationTests/TestResults/TestResults.trx
+dotnet test Tests/Letterbook.IntegrationTests/ --logger "trx;LogFileName=TestResults.trx" --logger "html;LogFileName=TestResults.html"
