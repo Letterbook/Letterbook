5ae568c668733c68883b0d68f042c2fb5807d505
Ben Biddington <ben.biddington@gmail.com>
2026-01-21 16:11:44 +1300

    Use the Github Actions logger

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -2,4 +2,7 @@
 set -e
 
 # Test results go to Tests/Letterbook.IntegrationTests/TestResults/TestResults.trx
-dotnet test Tests/Letterbook.IntegrationTests/ --logger "trx;LogFileName=TestResults.trx" --logger "html;LogFileName=TestResults.html"
+dotnet test Tests/Letterbook.IntegrationTests/ \
+	--logger "trx;LogFileName=TestResults.trx" \
+	--logger "html;LogFileName=TestResults.html" \
+	--logger "GitHubActions"
