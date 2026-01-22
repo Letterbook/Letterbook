97889dd78ce429ba810fa6dae102a86ba55c10a6
Ben Biddington <ben.biddington@gmail.com>
2026-01-22 13:21:01 +1300

    Allow running subset of test on build server

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+#
+# Run with filter
+#
+#	./scripts/integration-test.sh --filter CreatePost_Mastodon
+#
 # Test results go to Tests/Letterbook.IntegrationTests/TestResults/TestResults.trx
-dotnet test Tests/Letterbook.IntegrationTests/ \
-	--logger "trx;LogFileName=TestResults.trx" \
+dotnet test Tests/Letterbook.IntegrationTests/   \
+	--logger "trx;LogFileName=TestResults.trx"   \
 	--logger "html;LogFileName=TestResults.html" \
-	--logger "GitHubActions"
+	--logger "GitHubActions" $@
