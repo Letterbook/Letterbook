6338d0cf3d4fc62966ef2b54dc4d37407a9dd7a3
Ben Biddington <ben.biddington@gmail.com>
2026-02-05 16:48:18 +1300

    Fake all domain services and bypass authentication

--- Tests/Letterbook.IntegrationTests/Fixtures/ApiFixture.cs
+++ Tests/Letterbook.IntegrationTests/Fixtures/ApiFixture.cs
@@ -1,29 +1,35 @@
-﻿using Letterbook.Core;
+﻿using Letterbook.AspNet.Tests.Fixtures;
+using Letterbook.Core;
+using Letterbook.Core.Adapters;
 using Letterbook.Core.Workers;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Moq;
+using IAuthorizationService = Letterbook.Core.IAuthorizationService;
 
 namespace Letterbook.IntegrationTests.Fixtures;
 
 /// <summary>
-/// A web application factory that allows services to be replaced before startup.
+/// A fixture that allows services to be replaced before startup. It is intended to isolate the network adapter part from the application core.
 ///
-/// [!] Modifying HostFixture to do this would require too many changes at the moment.
+/// Differs from HostFixture in that all services are faked. HostFixture is more of an end-to-end, in-memory hosted style.
 ///
-/// The reason it doesn't work is because it calls `CreateScope` in its constructor.
-/// This immediately starts the application which means `ConfigureWebHost` is also called as part of constructor.
+/// Apart from that it is not possible currently to replace services post construction because the constructor calls CreateScope.
 ///
-/// There is then no longer an opportunity to replace services because `ConfigureWebHost` is only called once per start-up.
+/// CreateScope starts the application and therefore calls HostFixture.ConfigureWebHost.
 ///
-/// CustomWebApplicationFactory is different because you can replace services any time until `CreateApiClient` or `CreateClient` is called.
+/// ConfigureWebHost is called at most once so we lose the opportunity to configure services after construction.
 /// </summary>
 /// <param name="init"></param>
 public class ApiFixture : WebApplicationFactory<Program>
 {
 	public Mock<IAccountService> MockAccountService { get; } = new();
+	public Mock<IAuthorizationService> MockAuthorizationService { get; } = new(MockBehavior.Strict);
+	public Mock<IAuthzPostService> MockIAuthzPostService { get; } = new(MockBehavior.Strict);
+	public Mock<IPostService> MockPostService { get; } = new(MockBehavior.Strict);
 
 	private readonly List<Action<IServiceCollection>> _initializers = [];
 
@@ -38,15 +44,9 @@ public class ApiFixture : WebApplicationFactory<Program>
 
 		builder.ConfigureServices(s =>
 		{
-			/*
-
-				[!] Required otherwise Letterbook.IntegrationTests.InboxTests.ShouldAcceptReports fails.
-
-				Copied from HostFixture.
-
-			*/
-			var seedDescriptor = s.SingleOrDefault(d => d.ImplementationType == typeof(WorkerScope<SeedAdminWorker>));
-			if (seedDescriptor != null) s.Remove(seedDescriptor);
+			UnplugAdapterServices();
+			FakeDomainServices();
+			FakeAuthentication(s);
 
 			foreach (var action in _initializers)
 			{
@@ -55,7 +55,76 @@ public class ApiFixture : WebApplicationFactory<Program>
 		});
 	}
 
-	public void ReplaceScoped<TServiceType>(TServiceType replacement) where TServiceType : class
+	// https://learn.microsoft.com/en-us/aspnet/core/security/authorization/policies?view=aspnetcore-10.0
+	private void FakeAuthentication(IServiceCollection services)
+	{
+		services.AddAuthentication()
+			.AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", _ => { });
+
+		// See ProfileIdentityMiddleware
+		MockAccountService.Setup(it => it.LookupAccount(It.IsAny<Guid>())).ReturnsAsync(new Models.Account());
+
+		// [!] Without this, requests are redirected to log-in screen for some reason.
+		services.ConfigureApplicationCookie(options =>
+		{
+			options.ForwardAuthenticate = "Test";
+		});
+	}
+
+	protected override void ConfigureClient(HttpClient client)
+	{
+		base.ConfigureClient(client);
+
+		client.DefaultRequestHeaders.Authorization = new(
+			"Test",
+			// @todo: show that this MUST be a guid. See ProfileIdentityMiddleware for where it fails.
+			"86b77ec9-20d8-45b7-bd5d-d33cf8f6336c");
+	}
+
+	private void FakeDomainServices()
+	{
+		ReplaceScoped(new Mock<IAccountEventPublisher>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IActivityPubAuthenticatedClient>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IActivityPubClient>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IActivityPubDocument>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IActivityScheduler>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IApCrawlScheduler>(MockBehavior.Strict).Object);
+		ReplaceScoped(MockIAuthzPostService.Object);
+		ReplaceScoped(MockAuthorizationService.Object);
+		ReplaceScoped(new Mock<IFeedsAdapter>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IGlobalSearchProvider>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IHostSigningKeyProvider>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IModerationEventPublisher>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IPostEventPublisher>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IPostSearchProvider>(MockBehavior.Strict).Object);
+		ReplaceScoped(MockPostService.Object);
+		ReplaceScoped(new Mock<IProfileEventPublisher>(MockBehavior.Strict).Object);
+		ReplaceScoped(new Mock<IProfileSearchProvider>(MockBehavior.Strict).Object);
+	}
+
+	/// <summary>
+	/// Adapters services are dependencies OF dependencies and should never be invoked because the domain service layer has been replaced.
+	///
+	/// For example a controller would never invoke IDataAdapter directly.
+	///
+	/// These are configured to fail if used at all.
+	/// </summary>
+	private void UnplugAdapterServices()
+	{
+		ReplaceScoped(new Mock<IDataAdapter>(MockBehavior.Strict).Object);
+
+		/*
+
+			[!] Required otherwise Letterbook.IntegrationTests.InboxTests.ShouldAcceptReports fails.
+
+			Copied from HostFixture.
+
+		*/
+		var seedDescriptor = s.SingleOrDefault(d => d.ImplementationType == typeof(WorkerScope<SeedAdminWorker>));
+		if (seedDescriptor != null) s.Remove(seedDescriptor);
+	}
+
+	private void ReplaceScoped<TServiceType>(TServiceType replacement) where TServiceType : class
 	{
 		_initializers.Add(s =>
 		{
