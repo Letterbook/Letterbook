d4cf0784652573c093c3b690fe242309f127cef5
Jenniferplusplus <contact@jenniferplusplus.com>
2026-02-15 01:51:10 -0600

    wip add newly discovered objects to db

--- Source/Letterbook.Core/SearchService.cs
+++ Source/Letterbook.Core/SearchService.cs
@@ -1,20 +1,30 @@
 using System.Runtime.CompilerServices;
 using System.Security.Claims;
+using System.Text.RegularExpressions;
 using Letterbook.Core.Adapters;
+using Letterbook.Core.Extensions;
 using Letterbook.Core.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Letterbook.Core;
 
-public class SearchService : ISearchService, ISearchServiceAuth
+public partial class SearchService : ISearchService, ISearchServiceAuth
 {
+	private readonly ILogger<SearchService> _logger;
 	private readonly IEnumerable<ISearchProvider> _providers;
 	private readonly IAuthorizationService _authz;
+	private readonly IDataAdapter _data;
 	private IEnumerable<Claim> _claims = default!;
+	private readonly CoreOptions _opts;
 
-	public SearchService(IEnumerable<ISearchProvider> providers, IAuthorizationService authz)
+	public SearchService(ILogger<SearchService> logger, IEnumerable<ISearchProvider> providers, IAuthorizationService authz, IDataAdapter data, IOptions<CoreOptions> opts)
 	{
+		_logger = logger;
 		_providers = providers;
 		_authz = authz;
+		_data = data;
+		_opts = opts.Value;
 	}
 
 	public ISearchServiceAuth As(IEnumerable<Claim> claims)
@@ -25,18 +35,88 @@ public class SearchService : ISearchService, ISearchServiceAuth
 
 	public async IAsyncEnumerable<IFederated> SearchAll(string query, [EnumeratorCancellation] CancellationToken cancel, int limit = 100)
 	{
+		var matches = LocalHandleRegex().Matches(query);
+		if (matches.Count != 0)
+		{
+			var found = false;
+			await foreach (var p in _data.AllProfiles().Where(p => p.Handle == matches[0].Value && p.Authority == _opts.BaseUri().GetAuthority()).ToAsyncEnumerable().WithCancellation(cancel))
+			{
+				found = true;
+				yield return p;
+			}
+			if(found)
+				yield break;
+		}
+
+		// @handle@host - search local db profiles and providers, exit early
+		if (UriExtensions.TryParseHandle(query, out var handle, out var host))
+		{
+			var found = false;
+			await foreach (var p in _data.AllProfiles().Where(p => p.Handle == handle && p.Authority == host.GetAuthority()).ToAsyncEnumerable().WithCancellation(cancel))
+			{
+				found = true;
+				yield return p;
+			}
+			if (found)
+				yield break;
+		}
+
+		// uri://something - search local db profiles, posts, and providers, exit early
+		if (Uri.TryCreate(query, UriKind.Absolute, out var queryUri))
+		{
+			var found = false;
+			await foreach (var p in _data.Profiles(queryUri).ToAsyncEnumerable().WithCancellation(cancel))
+			{
+				found = true;
+				yield return p;
+			}
+			if (!found)
+			{
+				await foreach (var p in _data.Posts(queryUri).ToAsyncEnumerable().WithCancellation(cancel))
+				{
+					found = true;
+					yield return p;
+				}
+			}
+
+			if (found)
+				yield break;
+		}
+
+		var pendingData = false;
+		// anything else - search providers, then add to db
 		foreach (var provider in _providers)
 		{
 			var result = await provider.SearchAny(query, cancel);
-			foreach (var profile in result)
+			foreach (var resource in result)
 			{
+
 				limit--;
-				if (_authz.View(_claims, profile))
-					yield return profile;
+				switch (resource)
+				{
+					case Post post:
+						pendingData = true;
+						_logger.LogDebug("Adding post {Id} to local data from search", post.FediId);
+						_data.Add(post);
+						break;
+					case Profile profile:
+						pendingData = true;
+						_logger.LogDebug("Adding profile {Id} to local data from search", profile.FediId);
+						_data.Add(profile);
+						break;
+					default:
+						_logger.LogDebug("Search returned type {Type} {Id}", resource.GetType().ToString(), resource.FediId);
+						continue;
+				}
+				if (_authz.View(_claims, resource))
+					yield return resource;
 			}
 
-			if (limit <= 0) yield break;
+			if (limit <= 0) break;
 		}
+
+		if (pendingData)
+			await _data.Commit();
 	}
 
 	public async IAsyncEnumerable<Profile> SearchProfiles(string query, [EnumeratorCancellation] CancellationToken cancel, int limit = 100)
@@ -70,4 +150,11 @@ public class SearchService : ISearchService, ISearchServiceAuth
 			if (limit <= 0) yield break;
 		}
 	}
+
+	// match any string that begins with '@' and includes one or more characters other than '@'
+	// I'm sure other software disagrees, but there should be no reason we can't support literally any unicode character as part of a
+	// profile handle, except the special character @ which separates the username from the hostname
+	[GeneratedRegex("""(?<=^@)[^@]+$""")]
+	// [GeneratedRegex("""(?<=^@)\w+$""")]
+	private static partial Regex LocalHandleRegex();
 }
--- Tests/Letterbook.Core.Tests/SearchServiceTests.cs
+++ Tests/Letterbook.Core.Tests/SearchServiceTests.cs
@@ -1,5 +1,8 @@
 using Letterbook.Core.Adapters;
+using Letterbook.Core.Models;
 using Letterbook.Core.Tests.Fakes;
+using Microsoft.Extensions.Logging;
+using MockQueryable;
 using Moq;
 using Xunit.Abstractions;
 
@@ -9,13 +12,20 @@ public class SearchServiceTests : WithMocks
 {
 	private readonly SearchService _service;
 	private List<ISearchProvider> _providers = [];
-	private FakeProfile _profiles;
+	private FakeProfile _localProfiles;
+	private FakeProfile _peerProfiles;
+	private readonly FakePost _localPosts;
+	private readonly FakePost _peerPosts;
 
 	public SearchServiceTests(ITestOutputHelper output)
 	{
 		output.WriteLine($"Bogus seed: {Init.WithSeed()}");
-		_profiles = new FakeProfile();
-		_service = new SearchService(_providers, AuthorizationServiceMock.Object);
+		_localProfiles = new FakeProfile(CoreOptionsMock.Value.DomainName);
+		_localPosts = new FakePost(CoreOptionsMock.Value.DomainName);
+		_peerProfiles = new FakeProfile();
+		_peerPosts = new FakePost("peer.example");
+		_service = new SearchService(Mock.Of<ILogger<SearchService>>(), _providers, AuthorizationServiceMock.Object, DataAdapterMock.Object,
+			CoreOptionsMock);
 
 		MockAuthorizeAllowAll();
 	}
@@ -45,7 +55,7 @@ public class SearchServiceTests : WithMocks
 	{
 		var mocks = new List<Mock<ISearchProvider>> { new(), new() };
 		mocks[0].Setup(m => m.SearchProfiles(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()))
-			.ReturnsAsync(_profiles.Generate(1));
+			.ReturnsAsync(_localProfiles.Generate(1));
 		_providers.AddRange(mocks.Select(m => m.Object));
 
 		var actual = await _service.As([]).SearchProfiles("query", CancellationToken.None, 1).ToListAsync();
@@ -61,7 +71,7 @@ public class SearchServiceTests : WithMocks
 		foreach (var mock in mocks)
 		{
 			mock.Setup(m => m.SearchProfiles(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()))
-				.ReturnsAsync(_profiles.Generate(1));
+				.ReturnsAsync(_localProfiles.Generate(1));
 		}
 		_providers.AddRange(mocks.Select(m => m.Object));
 
@@ -75,11 +85,112 @@ public class SearchServiceTests : WithMocks
 	{
 		var mocks = new List<Mock<ISearchProvider>> { new(), new() };
 		mocks[0].Setup(m => m.SearchProfiles(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()))
-			.ReturnsAsync(_profiles.Generate(5));
+			.ReturnsAsync(_localProfiles.Generate(5));
 		_providers.AddRange(mocks.Select(m => m.Object));
 
 		var actual = await _service.As([]).SearchProfiles("query", CancellationToken.None, 1).ToListAsync();
 
 		Assert.Equal(5, actual.Count);
+	}
+
+	/***
+	 * SearchAll
+	 */
+
+	[Fact(DisplayName = "SearchAll should exit early on local handle")]
+	public async Task AllShouldExitLocal()
+	{
+		var profiles = _localProfiles.Generate(1);
+		var provider = new Mock<ISearchProvider>();
+		_providers.Add(provider.Object);
+		DataAdapterMock.Setup(m => m.AllProfiles()).Returns(profiles.BuildMock());
+
+		var actual = await _service.As([]).SearchAll($"@{profiles[0].Handle}", CancellationToken.None).ToListAsync();
+		Assert.Single(actual);
+
+		provider.Verify(p => p.SearchAny(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()), Times.Never);
+	}
+
+	[Fact(DisplayName = "SearchAll should exit early on full handle")]
+	public async Task AllShouldExitFullHandle()
+	{
+		var profiles = _localProfiles.Generate(1);
+		var provider = new Mock<ISearchProvider>();
+		_providers.Add(provider.Object);
+		DataAdapterMock.Setup(m => m.AllProfiles()).Returns(profiles.BuildMock());
+
+		var actual = await _service.As([]).SearchAll($"@{profiles[0].Handle}@{CoreOptionsMock.Value.DomainName}", CancellationToken.None).ToListAsync();
+		Assert.Single(actual);
+
+		provider.Verify(p => p.SearchAny(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()), Times.Never);
+	}
+
+	[Fact(DisplayName = "SearchAll should exit early on profile Uri")]
+	public async Task AllShouldExitProfileUri()
+	{
+		var profiles = _localProfiles.Generate(1);
+		var provider = new Mock<ISearchProvider>();
+		_providers.Add(provider.Object);
+		DataAdapterMock.Setup(m => m.Profiles(It.IsAny<Uri[]>())).Returns(profiles.BuildMock());
+
+		var actual = await _service.As([]).SearchAll(profiles[0].FediId.ToString(), CancellationToken.None).ToListAsync();
+		Assert.Single(actual);
+
+		provider.Verify(p => p.SearchAny(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()), Times.Never);
+	}
+
+	[Fact(DisplayName = "SearchAll should exit early on post Uri")]
+	public async Task AllShouldExitPostUri()
+	{
+		var posts = _localPosts.Generate(1);
+		var provider = new Mock<ISearchProvider>();
+		_providers.Add(provider.Object);
+		DataAdapterMock.Setup(m => m.Posts(It.IsAny<Uri[]>())).Returns(posts.BuildMock());
+
+		var actual = await _service.As([]).SearchAll(posts[0].FediId.ToString(), CancellationToken.None).ToListAsync();
+		Assert.Single(actual);
+
+		provider.Verify(p => p.SearchAny(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()), Times.Never);
+	}
+
+	[InlineData("@handle")]
+	[InlineData("@handle@host")]
+	[InlineData("https://host.example/value")]
+	[InlineData("any other text")]
+	[Theory(DisplayName = "SearchAll should call providers")]
+	public async Task AllShouldCallProviders(string query)
+	{
+		var provider = new Mock<ISearchProvider>();
+		_providers.Add(provider.Object);
+
+		await _service.As([]).SearchAll(query, CancellationToken.None).ToListAsync();
+
+		provider.Verify(p => p.SearchAny(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()), Times.Once);
+	}
+
+	[Fact(DisplayName = "SearchAll should add profiles")]
+	public async Task AllShouldAddProfileToDb()
+	{
+		var provider = new Mock<ISearchProvider>();
+		provider.Setup(p => p.SearchAny(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()))
+			.ReturnsAsync(_peerProfiles.Generate(1));
+		_providers.Add(provider.Object);
+
+		await _service.As([]).SearchAll("query", CancellationToken.None).ToListAsync();
+
+		DataAdapterMock.Verify(m => m.Add(It.IsAny<Profile>()), Times.Once);
+	}
+
+	[Fact(DisplayName = "SearchAll should add profiles")]
+	public async Task AllShouldAddPostsToDb()
+	{
+		var provider = new Mock<ISearchProvider>();
+		provider.Setup(p => p.SearchAny(It.IsAny<string>(), It.IsAny<CancellationToken>(), It.IsAny<int>()))
+			.ReturnsAsync(_peerPosts.Generate(1));
+		_providers.Add(provider.Object);
+
+		await _service.As([]).SearchAll("query", CancellationToken.None).ToListAsync();
+
+		DataAdapterMock.Verify(m => m.Add(It.IsAny<Post>()), Times.Once);
 	}
 }
