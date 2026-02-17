3759c230f8446d2892b7a11c83216690ccc3d3d9
Ben Biddington <ben.biddington@gmail.com>
2026-02-17 16:18:31 +1300

    [PASSES] GET to /lb/v1/search_profiles should call the correct search method

---
+++ Source/Letterbook.Api/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using Letterbook.Core;
+using Letterbook.Core.Adapters;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Letterbook.Api.Controllers;
+
+[Route("lb/v1")]
+public class SearchController(ISearchProvider searchProvider): ControllerBase
+{
+	[HttpGet("search_profiles")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[SwaggerOperation("Search", "Search for a profile")]
+	public IActionResult SearchProfiles([FromQuery(Name = "q")] string query)
+	{
+		searchProvider.SearchProfiles(query, CancellationToken.None, new CoreOptions
+		{
+
+		}, 100);
+		return Ok();
+	}
+}
