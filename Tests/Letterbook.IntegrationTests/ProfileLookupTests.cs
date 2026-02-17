e0038129c3c702e568cb07601ffe8be41b6f7a21
Ben Biddington <ben.biddington@gmail.com>
2026-02-17 15:32:49 +1300

    [FAILS] GET to /lb/v1/search_profiles should call the correct search method

---
+++ Tests/Letterbook.IntegrationTests/ProfileLookupTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Letterbook.IntegrationTests.Fixtures;
+using Xunit.Abstractions;
+
+namespace Letterbook.IntegrationTests;
+
+public class ProfileLookupTests(ApiFixture fixture, ITestOutputHelper log) : IClassFixture<ApiFixture>
+{
+	[Fact(DisplayName = "Should invoke search all profile")]
+	public async Task InvokeCorrectSearchMethod()
+	{
+		using var _client = fixture.CreateClient();
+
+		var response = await _client.GetAsync("/lb/v1/search_profiles?q=ben@letterbook.social");
+
+		var body = await response.Content.ReadAsStringAsync();
+
+		log.WriteLine(body);
+
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+	}
+
+	// TEST: is authentication required?
+}
