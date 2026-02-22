367c79429e7a49baff9a8cef63b6565012478fb8
Ben Biddington <ben.biddington@gmail.com>
2026-02-22 14:26:54 +1300

    Show that DataAdapter.SearchProfiles works with '@handle@host' and '@handle'

--- Tests/Letterbook.IntegrationTests/DataAdapterTests.cs
+++ Tests/Letterbook.IntegrationTests/DataAdapterTests.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
+using OpenIddict.Abstractions;
 using Xunit.Abstractions;
 
 namespace Letterbook.IntegrationTests;
@@ -96,5 +97,29 @@ public sealed class DataAdapterTests : IClassFixture<HostFixture<DataAdapterTest
 		Assert.True(adHocProjection.Followers >= 4);
 		Assert.True(automapProjection.Followers >= 4);
 		Assert.Equal(0, noProjection.FollowersCount);
+	}
+
+	[Fact(DisplayName = "Search profile example: @handle")]
+	public async Task ProfileSearchExamples()
+	{
+		var localProfile = _profiles[0];
+
+		var actualProfile = Assert.Single(
+			await _adapter.SearchProfiles(
+				$"@{localProfile.Handle}", CancellationToken.None, new CoreOptions()));
+
+		Assert.Equal(localProfile.Handle, actualProfile.Handle);
+	}
+
+	[Fact(DisplayName = "Search profile example: @handle@host")]
+	public async Task ProfileSearchExamplesHandleAndHost()
+	{
+		var localProfile = _profiles[0];
+
+		var actualProfile = Assert.Single(
+			await _adapter.SearchProfiles(
+				$"@{localProfile.Handle}@{localProfile.Authority}", CancellationToken.None, new CoreOptions()));
+
+		Assert.Equal(localProfile.Handle, actualProfile.Handle);
 	}
 }
